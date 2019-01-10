e9f2bb6f9a8d25185c95ac8c77fc1d2e8b6b52b7
Philippe Marschall <philippe.marschall@netcetera.com>
2019-01-10 10:36:04 +0100

    updates

--- README.md
+++ README.md
@@ -6,7 +6,18 @@ A reimplementation of [paigen11/read-file-java](https://github.com/paigen11/read
 We use the following approach for parsing
 
 * Use [marschall/mini-csv](https://github.com/marschall/mini-csv) for CSV parsing, which uses [marschall/line-parser](https://github.com/marschall/line-parser).
-  * This allows us to drastically cut down on string allocations.
-* Use an Eclipse Collections Bag or counting it occurrences of months and first names.
+  * This allows us to drastically cut down on string allocations as just a reused CharSequence view is created for every line instead of a full String.
+  * Since the file is in ASCI we can safe us the decoding and turn every byte into a char.
+* Use an [Eclipse Collections](https://www.eclipse.org/collections/) [Bag](https://github.com/eclipse/eclipse-collections/blob/master/docs/guide.md#-bag) or counting it occurrences of months and first names.
+  * This allows us to not to have to hold on to every first name and is more efficient than a `HashMap<String, Integer>`.
+  * Unfortunately this adds about 10 MB.
 * Use YearMonth instead of a formatted String for representing a month.
   * Use Integer.parseInt for parsing the YearMonth instead of DateTimeFormatterBuilder because is drastically cuts down on allocations. This causes a noticeable speed improvement.
+
+```
+time java -Xmx16m -cp target/read-file-java-0.1.0-SNAPSHOT-shaded.jar com.github.marschall.readfilejava.ReadFile /path/to/file
+``
+
+```
+time java -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler
+``
--- src/main/java/com/github/marschall/readfilejava/ReadFile.java
+++ src/main/java/com/github/marschall/readfilejava/ReadFile.java
@@ -23,16 +23,15 @@ public class ReadFile {
       System.err.println("usage: file");
       System.exit(-1);
     }
-    
-    long start = System.currentTimeMillis();
-    var csvParser = new CsvParser('|');;
+
+    var csvParser = new CsvParser('|');
     var path = Paths.get(args[0]);
     var parseContext = new ParseContext();
-    
+
     csvParser.parse(path, US_ASCII, parseContext);
-    
+
     System.out.println("Total file line count: " + parseContext.getLineCount());
-    
+
     MutableBag<YearMonth> donations = parseContext.getDonations();
     donations.forEachWithOccurrences((donation, occurrence) -> {
       System.out.println("Donations per month and year: "
@@ -42,92 +41,96 @@ public class ReadFile {
     ObjectIntPair<String> mostCommonName = parseContext.getMostCommonName();
     System.out.println("The most common first name is: " + mostCommonName.getOne()
       + " and it occurs: " + mostCommonName.getTwo() + " times.");
-    
-    System.out.println(System.currentTimeMillis() - start);
   }
-  
+
   static final class ParseContext implements Consumer<Row> {
-    
+
     private long lineCount;
-    
+
     private MutableBag<YearMonth> donations;
-    
+
     private MutableBag<String> firstNames;
-    
+
     ParseContext() {
       this.lineCount = 0L;
       this.donations = new HashBag<>();
       this.firstNames = new HashBag<>();
     }
-    
+
     MutableBag<String> getFirstNames() {
       return this.firstNames.asUnmodifiable();
     }
-    
+
     MutableBag<YearMonth> getDonations() {
       return this.donations.asUnmodifiable();
     }
-    
+
     private void incrementLineCount() {
       this.lineCount++;
     }
-    
+
     private void addDonation(YearMonth donationMonth) {
       this.donations.add(donationMonth);
     }
-    
+
     private void addFristName(String firstName) {
       this.firstNames.add(firstName);
     }
-    
+
     private long getLineNumber() {
       return this.lineCount;
     }
-    
+
     long getLineCount() {
       return this.lineCount;
     }
-    
+
     ObjectIntPair<String> getMostCommonName() {
       return this.firstNames.topOccurrences(1).getFirst();
     }
-    
+
 
     @Override
     public void accept(Row row) {
       this.incrementLineCount();
-      
+
       CellSet cellSet = row.getCellSet();
       while (cellSet.next()) {
         int columnIndex = cellSet.getColumnIndex();
         if (columnIndex == 4) {
-          YearMonth dondationMonth = parseDonation(cellSet.getCharSequence());
-          this.addDonation(dondationMonth);
+          readDonationMonth(cellSet.getCharSequence());
         } else if (columnIndex == 7) {
-          CharSequence name = cellSet.getCharSequence();
-
-          long lineNumber = this.getLineNumber(); // 1 based
-          if (lineNumber == 432L || lineNumber == 43243L) {
-            System.out.println("Name: " + name + " at index: " + lineNumber);
-          }
-          
-          String firstName = extractFirstName(name);
-          if (firstName != null) {
-            this.addFristName(firstName);
-          }
+          readName(cellSet.getCharSequence());
         } else if (columnIndex > 7) {
           break;
         }
       }
     }
-    
+
+    private void readName(CharSequence name) {
+      long lineNumber = this.getLineNumber(); // 1 based
+      if (lineNumber == 432L || lineNumber == 43243L) {
+        System.out.println("Name: " + name + " at index: " + lineNumber);
+      }
+
+      String firstName = extractFirstName(name);
+      if (firstName != null) {
+        this.addFristName(firstName);
+      }
+    }
+
+    private void readDonationMonth(CharSequence cellValue) {
+      YearMonth dondationMonth = parseDonation(cellValue);
+      this.addDonation(dondationMonth);
+    }
+
     static YearMonth parseDonation(CharSequence donation) {
       // use this because of DateTimeFormatterBuilder because it allocates a lot less
       int year = Integer.parseInt(donation, 0, 4, 10);
       int month = Integer.parseInt(donation, 4, 6, 10);
       return YearMonth.of(year, month);
     }
-    
+
     static String extractFirstName(CharSequence name) {
       // "PEREZ, JOHN A" -> "JOHN"
       int semiColon = CharSequences.indexOf(name, ',');
@@ -148,7 +151,7 @@ public class ReadFile {
         return name.subSequence(start, name.length()).toString();
       }
     }
-    
+
   }
 
 }
